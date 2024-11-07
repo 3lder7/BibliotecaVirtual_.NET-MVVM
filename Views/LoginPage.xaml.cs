c52752e93abf9802e4fcb646e39954886384ef6f
Elder <eldergalvao07@gmail.com>
2024-11-06 21:57:53 -0300

    aa

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -1,9 +1,31 @@
-namespace bibliotecaVirtual_com_MVVM_.Views;
+using Microsoft.Maui.Controls;
 
-public partial class LoginPage : ContentPage
+namespace bibliotecaVirtual_com_MVVM_.Views
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnEntrarClicked(object sender, EventArgs e)
+        {
+            string email = EmailEntry.Text;
+            string senha = SenhaEntry.Text;
+
+            if (!string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(senha))
+            {
+                // adicionar lógica para verificar as credenciais do usuário
+
+                // mensagem temporária de sucesso
+                await DisplayAlert("Login", "Login realizado com sucesso!", "OK");
+            }
+            else
+            {
+                //mensagem de erro se o usuário não preencher todos os campos
+                await DisplayAlert("Erro", "Por favor, insira o email e a senha.", "OK");
+            }
+        }
+    }
 }
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/bibliotecaVirtual(com MVVM).AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/bibliotecaVirtual(com MVVM).AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bibliotecaVirtual(com MVVM)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8478d6f5a9763b53d491fda3962cf143e9b2eff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+08e2e81fe10cbe75a5e51e4301972a041e704f3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("bibliotecaVirtual(com MVVM)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bibliotecaVirtual(com MVVM)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
