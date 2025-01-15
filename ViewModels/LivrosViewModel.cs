bed3f3891b7498405f71930a7a3e628029f2d57c
Elder <eldergalvao07@gmail.com>
2025-01-15 17:15:36 -0300

    Atualizando LivroViewModel

--- ViewModels/LivrosViewModel.cs
+++ ViewModels/LivrosViewModel.cs
@@ -1,24 +1,34 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel; //necessário
-
+﻿using System.Collections.ObjectModel;
+using bibliotecaVirtual_com_MVVM_.Models;
 
 namespace bibliotecaVirtual_com_MVVM_.ViewModels
 {
-    public class LivrosViewModel
+    public class MainPageViewModel
     {
-        public ObservableCollection<Livro> Livros { get; set; }
+        public ObservableCollection<Livros.Livro> Livros { get; set; }
 
-        public LivrosViewModel() 
-        { 
-            Livros = new ObservableCollection<Livro>
+        public MainPageViewModel()
+        {
+            //livros previamente cadastrados
+            Livros = new ObservableCollection<Livros.Livro>
             {
-
-            }
+                new Livros.Livro
+                {
+                    Titulo = "O Hobbit",
+                    Autor = "J.R.R. Tolkien",
+                    ImagemUrl = "https://linkparaimagem.com/ohobbit.jpg",
+                    Status = "Lido",
+                    qtdPagina = 310
+                },
+                new Livros.Livro
+                {
+                    Titulo = "Dom Casmurro",
+                    Autor = "Machado de Assis",
+                    ImagemUrl = "https://linkparaimagem.com/domcasmurro.jpg",
+                    Status = "Em leitura",
+                    qtdPagina = 256
+                }
+            };
         }
     }
 }
-*/
--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/bibliotecaVirtual(com MVVM).AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/bibliotecaVirtual(com MVVM).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bibliotecaVirtual(com MVVM)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4743704a80e91cdd865a5c7285061f8c7003af52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d081a1ce4ea8163b6461053d00d6d97500272a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("bibliotecaVirtual(com MVVM)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bibliotecaVirtual(com MVVM)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
